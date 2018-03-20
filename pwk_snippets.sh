9c98fd9f4a9d8ac41a6ccaf6aa382cad43bd43a4
DanieleCapponi <d.danielecapponi@gmail.com>
2018-03-20 18:50:52 +0100

    commit2

--- pwk_snippets.sh
+++ pwk_snippets.sh
@@ -3,12 +3,26 @@
 #***************************************************************************
 
 # KALI MENU
-updatedb			# builds a FileSystem's database
-locate sbd.exe			# locates files in db
-which sbd			# searches through $PATH-defined directories
-find / -name sbd*		# searches recursively in any given path (most aggressive)
+updatedb							# builds a FileSystem's database
+
+locate sbd.exe						# locates files in db
+which sbd							# searches through $PATH-defined directories
+find / -name sbd*					# searches recursively in any given path (most aggressive)
+
 
 # KALI MANAGE
-passwd				# Changes default kali password
-service ssh start 		# Starting the SSH service
-netstat -antp | grep sshd		# searches output for ssh
+passwd								# Changes default kali password
+
+service ssh start 					# Starting the SSH service
+netstat -antp | grep sshd			# searches output for ssh
+
+update-rc.d ssh enable				# starts automatically at boot time ssh service
+
+service apache2 start 				# starts HTTP service
+
+
+# BASH ENVIRONMENT
+wget https://www.cisco.com			# downloads web page
+ls -l index -html					# more details than ls
+
+grep "href=" index.html | cut -d "/" -f 3 | grep "\." | cut -d '"' -f 1 | sort -u
