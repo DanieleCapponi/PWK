1840f80cd4e5dc2ab2fe2a11d83212bcc686cea1
DanieleCapponi <d.danielecapponi@gmail.com>
2018-03-21 00:21:17 +0100

    cap 2 cripts

--- scripts/C1.4.3_IPsweep.sh
+++ scripts/C1.4.3_IPsweep.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
+IP="192.168.0."
 
+echo "" > temp
+
+echo starting IP sweep over subnet...
+echo
+
+for val in {1..254}; do ((ping -c 10 -i 0.1 $IP$val &) >> temp) >> temp; done
+ 
+echo ---\> IPs_up.txt
+echo 
+	
+grep "from " temp | cut -d " " -f 4 | cut -d ":" -f 1 | sort -u > IPs_up.txt
+
+echo "***************" $(wc IPs_up.txt | cut -d " " -f 2) HOSTS UP "***************"
+
+cat IPs_up.txt
+echo
+rm temp
