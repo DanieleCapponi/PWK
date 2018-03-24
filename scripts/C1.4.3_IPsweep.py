a7a315e014c95cf0fec7d913ab537d8a60f47a2a
DanieleCapponi <d.danielecapponi@gmail.com>
2018-03-24 16:55:22 +0100

    C1 end

--- scripts/C1.4.3_IPsweep.py
+++ scripts/C1.4.3_IPsweep.py
@@ -1,18 +1,37 @@
-import sys
-import os
+#!/usr/bin/python
 
-ips = "192.168.254."
+import threading
+import time
+import socket
 
+class myThread (threading.Thread):
+	def __init__(self, threadID, name, counter):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.counter = counter
+			
+	def run(self):
+		ip = '192.168.0.'+str(self.counter)
+		port = 25
+		timeout = 3
+		
+		ports = {'tcpM':1, 'udp':7, 'ftpD':20, 'ftpC':21, 'ssh':22, 'tlnt':23, 'smtp':25, 'dns':53,
+		'dhcpS':67, 'dhcpC':68, 'http':80, 'kerb':88, 'pop3':110, 'imap4':143, 'https':443,
+		'smtp':465, 'syslog':514, 'smtp':587, 'imap4S':993, 'pop3S':995}
+		
+		for el in ports :
+			try:
+				socket.create_connection((ip, el), timeout)
+				print "[*]Connected :\t" + ip + ":" + el
+			except Exception, arg:
+				if (str(arg).find("refused", 0) > -1) :
+					print "[*]Host UP :\t" + ip + ":" + el
+					
+					
+						
 i = 1
-response, listed = "", ""
-os.system("echo '' > pingSweep.txt")
-
 while (i < 255):
-	response = (os.system("ping -c 2 -i 0.1 -q " + ips + str(i) + " >> pingSweep.txt"))
+	thread = myThread(i, "Thread_0."+str(i), i)
+	thread.start()
 	i = i+1
-	listed += " " + str(response)
-	print(response)
-	#if response.find("from"):
-	#	listed += ips + str(i)
-
-print (listed)
