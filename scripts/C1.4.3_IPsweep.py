3d6802a7f123a0fc50c5bd7933df04ea34641097
DanieleCapponi <d.danielecapponi@gmail.com>
2018-03-25 23:33:49 +0200

    cap 2

--- scripts/C1.4.3_IPsweep.py
+++ scripts/C1.4.3_IPsweep.py
@@ -4,7 +4,13 @@ import threading
 import time
 import socket
 
+global result
+
 class myThread (threading.Thread):
+	
+	result = ""
+	flag = 1
+	
 	def __init__(self, threadID, name, counter):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
@@ -23,15 +29,36 @@ class myThread (threading.Thread):
 		for el in ports :
 			try:
 				socket.create_connection((ip, el), timeout)
-				print "[*]Connected :\t" + ip + ":" + el
+				print "[*]Connected :\t" + ip + ": " + str(ports[el]) + " (" + el + ")"
+				output(ip, "[*]Connected :\t" + ip + ":" + el, el)
+				self.flag = 0
 			except Exception, arg:
 				if (str(arg).find("refused", 0) > -1) :
-					print "[*]Host UP :\t" + ip + ":" + el
+					if self.flag == 1:
+						print "[*]Host UP :\t" + ip
+					self.flag = 0
+			
+			
+	def output(ip, s, el):
+		if self.result.find(ip) > -1 :
+			self.result += "\t\t\t open port: " + el + "(" + str(el) + ")"
+		else :
+			self.result += "\n[*] IP: " + ip
+			
+		
+		
 					
-					
+threadLock = threading.Lock()
+threads = []				
 						
 i = 1
 while (i < 255):
 	thread = myThread(i, "Thread_0."+str(i), i)
 	thread.start()
+	threads.append(thread)
 	i = i+1
+	
+	
+for th in threads:
+	th.join()
+	
